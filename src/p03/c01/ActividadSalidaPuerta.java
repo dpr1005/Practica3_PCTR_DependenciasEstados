7850577fc647fbf80a98031f7411fdba5feea0c4
Daniel Puente Ramírez <dpr1005@alu.ubu.es>
2021-04-12 09:36:57 +0200

    Sistema Lanzador
    Hecho Sistema Lanzador y añadido Actividad Salida el constructor y como Runnable

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,28 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
 
-	//
+
+    public ActividadSalidaPuerta(String puerta, IParque parque) {
+    }
+
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+
+    //
 	// TODO
 	//
 }
