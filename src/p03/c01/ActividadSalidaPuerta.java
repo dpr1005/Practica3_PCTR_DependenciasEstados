1fceed60c51f508f7772fa4a8ec9682c12b4c315
Daniel Puente Ramírez <dpr1005@alu.ubu.es>
2021-04-12 09:45:20 +0200

    Implementada ActividadSalidaParque

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,9 +1,20 @@
 package src.p03.c01;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 public class ActividadSalidaPuerta implements Runnable{
 
+    // Declaración de variables
+    private static final int NUMSALIDAS = 20;
+    private String puerta;
+    private IParque parque;
 
     public ActividadSalidaPuerta(String puerta, IParque parque) {
+        this.puerta = puerta;
+        this.parque = parque;
     }
 
     /**
@@ -19,10 +30,16 @@ public class ActividadSalidaPuerta implements Runnable{
      */
     @Override
     public void run() {
-
+        for(int i = 0; i < NUMSALIDAS; i++) {
+            try{
+                TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+                parque.salirDelParque(puerta);
+            } catch (InterruptedException ex) {
+                Logger.getGlobal().log(Level.INFO, "Salida Interrumpida.");
+                Logger.getGlobal().log(Level.INFO, ex.toString());
+                return;
+            }
+        }
     }
 
-    //
-	// TODO
-	//
 }
